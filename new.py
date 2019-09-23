39f4d2ca2fa931c5dc97803aadaad5893e76eca3
xandrade <andrade.antonio@gmail.com>
2019-09-23 20:33:23 +0400

    .

--- new.py
+++ new.py
@@ -15,10 +15,6 @@ import numpy as np
 video_capture = cv2.VideoCapture(0)
 
 # Load a sample picture and learn how to recognize it.
-antonio_image = face_recognition.load_image_file("antonio.jpg")
-antonio_face_encoding = face_recognition.face_encodings(antonio_image)[0]
-
-# Load a sample picture and learn how to recognize it.
 obama_image = face_recognition.load_image_file("obama.jpg")
 obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
 
@@ -28,12 +24,10 @@ biden_face_encoding = face_recognition.face_encodings(biden_image)[0]
 
 # Create arrays of known face encodings and their names
 known_face_encodings = [
-    antonio_face_encoding,
     obama_face_encoding,
     biden_face_encoding
 ]
 known_face_names = [
-    "Me",
     "Barack Obama",
     "Joe Biden"
 ]
